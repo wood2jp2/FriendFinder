862a720bdd9c1e0c518daea6cc96c77adffc037f
Josh Wood <wood2jp2@gmail.com>
2017-08-20 20:22:07 -0400

    various routes added and html pages set up

--- app/server.js
+++ app/server.js
@@ -1,3 +1,36 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+var friends = [{
+  name: 'Josh',
+  photo: 'google.com',
+  scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}];
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/home.html"))
+});
+
+app.get('/survey', function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/survey.html"))
+});
+
+app.get('/api/friends', function(req, res) {
+  res.send(friends);
+});
+
+app.listen(PORT, function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
