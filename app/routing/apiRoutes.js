d427a3a23c9831969494be1b3ab8bde2cf0e69a0
Josh Wood <wood2jp2@gmail.com>
2017-08-21 23:43:56 -0400

    modal and heroku left

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,13 @@ module.exports = function(app) {
 
   app.post('/api/friends', function(req, res) {
 
-    var bestMatch;
+
     console.log(req.body);
     var pushedFriendScore = req.body.scores.reduce(function(sum, value) {
       return sum + value;
     }, req.body.scores[0]);
 
-    var matchToBeat;
+
     for (let i = 0; i < friendsData.length; i++) {
 
       var currentFriendScore = friendsData[i].scores.reduce(function(sum, value) {
@@ -23,13 +23,15 @@ module.exports = function(app) {
       var difference = Math.abs(currentFriendScore - pushedFriendScore);
 
       if (matchToBeat === 'undefined' || difference < matchToBeat) {
-        matchToBeat = difference;
-        bestMatch = friendsData[i];
+        var matchToBeat = difference;
+        var bestMatch = friendsData[i];
       }
     };
+    console.log(bestMatch);
 
     friendsData.push(req.body);
 
     res.json(bestMatch);
+
   });
 }
