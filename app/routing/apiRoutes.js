6561d9adfcda03dae74e14c6a713bcee28b998f2
Josh Wood <wood2jp2@gmail.com>
2017-08-21 23:09:28 -0400

    finally got the post to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,30 +6,30 @@ module.exports = function(app) {
   });
 
   app.post('/api/friends', function(req, res) {
-    res.send('Friend added');
-
-    friendsData.push(req.body);
 
     var bestMatch;
+    console.log(req.body);
     var pushedFriendScore = req.body.scores.reduce(function(sum, value) {
       return sum + value;
     }, req.body.scores[0]);
 
     var matchToBeat;
-    for (let i = 0; i < friendsData.length - 1; i++) {
+    for (let i = 0; i < friendsData.length; i++) {
 
       var currentFriendScore = friendsData[i].scores.reduce(function(sum, value) {
         return sum + value;
       }, friendsData[i].scores[0]);
 
       var difference = Math.abs(currentFriendScore - pushedFriendScore);
-      if (matchToBeat[0] === 'undefined' || difference < matchToBeat) {
+
+      if (matchToBeat === 'undefined' || difference < matchToBeat) {
         matchToBeat = difference;
         bestMatch = friendsData[i];
       }
-    }
+    };
 
+    friendsData.push(req.body);
 
-
+    res.json(bestMatch);
   });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,17 @@ const app = express();
 
 module.exports = function(app) {
 
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
-  });
 
   app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"))
   });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'))
+  });
+
+  // app.use(function(req, res) {
+  //   res.sendFile(path.join(__dirname, '../public/home.html'))
+  // });
+
 };
