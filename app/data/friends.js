028ead5ce23f58634ee73b6dd2deddb3e60aeabe
Josh Wood <wood2jp2@gmail.com>
2017-08-22 23:01:37 -0400

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,19 @@
 data = [{
-  name: 'Josh',
-  photo: 'google.com',
-  scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  name: 'Batman',
+  photo: 'http://vignette1.wikia.nocookie.net/batman/images/c/c8/Batman_%2766_-_Adam_West_as_Batman_2.jpg/revision/latest?cb=20140731220401',
+  scores: [1, 2, 3, 4, 5, 2, 3, 4, 5, 1]
 }, {
-  name: 'new',
-  photo: 'example',
-  scores: [3, 3, 8, 5, 5, 5, 4, 2, 2, 2]
+  name: 'Urkel',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/e/e0/Steve_Urkel.jpg',
+  scores: [3, 3, 1, 2, 5, 5, 4, 2, 2, 2]
+}, {
+  name: 'Elaine',
+  photo: 'http://cdn-wpmsa.defymedia.com/wp-content/uploads/sites/3/2014/05/elaine-benes-from-seinfeld.jpeg',
+  scores: [1, 2, 2, 1, 1, 4, 5, 3, 2, 1]
+}, {
+  name: 'Barney',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/d/de/Barney_Gumble.png',
+  scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 }];
 
 module.exports = data;
